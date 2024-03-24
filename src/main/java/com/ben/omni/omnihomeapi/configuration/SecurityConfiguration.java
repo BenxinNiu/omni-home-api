2a8026c5a05c2b470ebe089af425504c8f68fe35
Benxin Niu <bn2645@mun.ca>
2024-03-24 20:19:19 -0230

    setup securityFilterChain bean

--- src/main/java/com/ben/omni/omnihomeapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/ben/omni/omnihomeapi/configuration/SecurityConfiguration.java
@@ -1,17 +1,23 @@
 package com.ben.omni.omnihomeapi.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@ConditionalOnProperty(name = "security.enabled", havingValue = "true", matchIfMissing = true)
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authRequest -> authRequest
+        return http
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(authRequest -> authRequest
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/hello/**").permitAll()
                         .requestMatchers("/actuator/shutdown").denyAll()
